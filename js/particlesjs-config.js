76ca852a64690759e8183aa6534daaeed7c53456
qinxij <qinxij@163.com>
2017-01-06 23:37:25 +0800

    adjust particles color

--- js/particlesjs-config.js
+++ js/particlesjs-config.js
@@ -8,7 +8,7 @@ particlesJS("particles-js", {
       }
     },
     "color": {
-      "value": "#e37575"
+      "value": "#75e375"
     },
     "shape": {
       "type": "circle",
@@ -48,7 +48,7 @@ particlesJS("particles-js", {
     "line_linked": {
       "enable": true,
       "distance": 150,
-      "color": "#e37575",
+      "color": "#75e375",
       "opacity": 0.4,
       "width": 1
     },
